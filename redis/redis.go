9dbd29d4c28a25c91d60c26f68ff526dbbf25dfd
abdfnx <abdfnabd@gmail.com>
2021-12-29 16:47:01 +0300

    add `NewRedisClient` func in `redis` package, run `go mod tidy`

--- redis/redis.go
+++ redis/redis.go
@@ -1,6 +1,13 @@
 package redis
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/abdfnx/redui/core"
+	"github.com/abdfnx/redui/config"
+
+	"github.com/gdamore/tcell"
 	goRedis "github.com/go-redis/redis/v8"
 )
 
@@ -19,4 +26,38 @@ type RedisClient interface {
 	Process(cmd goRedis.Cmder) error
 	Do(args ...interface{}) *goRedis.Cmd
 	Info(section ...string) *goRedis.StringCmd
+}
+
+func NewRedisClient(conf config.Config, outputChan chan core.OutputMessage) RedisClient {
+	if conf.Cluster {
+		options := &goRedis.ClusterOptions{
+			Addrs:    []string{fmt.Sprintf("%s:%d", conf.Host, conf.Port)},
+			Password: conf.Password,
+		}
+
+		return goRedis.NewClusterClient(options)
+	}
+
+	options := &goRedis.Options{
+		Addr:         fmt.Sprintf("%s:%d", conf.Host, conf.Port),
+		DB:           conf.DB,
+		Password:     conf.Password,
+		WriteTimeout: 3 * time.Second,
+		ReadTimeout:  2 * time.Second,
+	}
+
+	client := goRedis.NewClient(options)
+	if conf.Debug {
+		client.WrapProcess(func(oldProcess func(cmd goRedis.Cmder) error) func(cmd goRedis.Cmder) error {
+			return func(cmd goRedis.Cmder) error {
+
+				outputChan <- core.OutputMessage{Color: tcell.ColorOrange, Message: fmt.Sprintf("redis: <%s>", cmd)}
+				err := oldProcess(cmd)
+
+				return err
+			}
+		})
+	}
+
+	return client
 }
